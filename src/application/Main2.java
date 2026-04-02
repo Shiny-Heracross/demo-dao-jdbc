eaa822bcb4fd23586e71f6e5b05d2d652dce3882
Shiny Heracross <macuinhacudepato@gmail.com>
2026-04-02 12:29:50 -0300

    departmentDaoJDBC findAll implementation

--- src/application/Main2.java
+++ src/application/Main2.java
@@ -1,5 +1,7 @@
 package application;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 import model.dao.DaoFactory;
@@ -11,11 +13,23 @@ public class Main2 {
 		DepartmentDao departmentDao = DaoFactory.createDepartmentDao();
 
 		Scanner sc = new Scanner(System.in);
-
+		List<Department> list = new ArrayList<>();
+		
+		
 		System.out.println("===== TEST 1: Department findById =====");
 		Department department = departmentDao.findById(3);
 		
 		System.out.println(department);
+		
+		System.out.println("\n===== TEST 2: Department findAll=====");
+		list = departmentDao.findAll();
+		
+		for(Department obj : list) {
+			System.out.println(obj);
+		}
+		
+		
+		
 		
 	sc.close();
 	}
